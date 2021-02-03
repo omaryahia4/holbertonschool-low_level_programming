3bd1a0ac31534124a68891e96f1762f095652629
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-03 19:48:23 +0000

    simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - Entry point
-*
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-  putchar("\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
