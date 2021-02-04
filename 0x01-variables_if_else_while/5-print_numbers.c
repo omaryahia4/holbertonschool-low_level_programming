1379bfcbb4fa06de49c2254be4a72b37762506f6
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-04 16:57:49 +0000

    from o to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints numbers from 0 to 9.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+putchar('\n');
+return (0);
