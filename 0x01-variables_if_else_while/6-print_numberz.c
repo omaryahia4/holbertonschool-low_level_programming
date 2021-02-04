5d311c0615087e9b9d461609fcf107b828305859
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-04 20:47:08 +0000

    from 0 to 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
 * main - Prints numbers between 0 to 9
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-int i;
-for (i = 48; i <= 57; ++i)
+int x;
+for(x = 48; x <= 57; ++x)
 {
-putchar(i);
+putchar(x);
 }
 putchar('\n');
 return (0);
