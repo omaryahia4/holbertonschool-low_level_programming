21321cec1848a4806280036229ff486c107aaee0
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-04 17:43:08 +0000

    backwards

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+/**
+* main - Prints numbers between 0 to 9.
+* Return: Always 0 (Success)
+*/
+int main(void)
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - printing alphabet backwards
+*Return: 0 in success
+*/
+int main(void)
+{
+char x;
+for (x = 'z'; x <= 'a'; x--)
+putchar(x);
+putchar('\n');
+return (0);
+}
