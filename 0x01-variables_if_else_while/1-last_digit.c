31f6550bc2f8f68c0d6beea22c4c531a5e9abf40
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-04 14:48:40 +0000

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
-* main - Determines either greater than 5, is less than 6 or is 0 
-* Return: Always 0
+* main - Determines either greater than 5, is less than 6, or is 0 
+* Return: Always 0 (Success)
 */ 
 int main(void)
 {
