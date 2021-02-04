da9f3e68c8b70ae8eb1388f98d6d2e4fe59b9204
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-04 12:53:00 +0000

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ Srand (time (0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("d% is positive\n", n);
+printf("%d is positive\n", n);
 }
 else (n == 0)
 {
-printf("d% is zero\n", n);
+printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("d% is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
