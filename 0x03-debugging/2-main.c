5596aa1cef1f0c6555169754f7ae3bfda11e40e4
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-09 22:18:57 +0000

    main largest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "holberton.h"
-
 /**
- * main - prints the largest of 3 integers
- * Return: 0
- */
+* main - prints the largest of 3 integers
+* Return: 0
+*/
 
 int main(void)
 {
