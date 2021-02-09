e835def34ea0fac1a52e2e5cfa2d9e7a5344088f
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-09 10:20:28 +0000

    x10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
-
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: Always 0 (Success)
- */
+*print_alphabet_x10 - print alphabet in 10 lines.
+*@c : input character
+*/
 void print_alphabet_x10(void)
 {
-  int i, c;
-
-  for (i = 0; i <= 9; i++)
-    {
-      for (c = 97; c <= 122; c++)
-	{
-	  _putchar(c);
-	}
-      _putchar('\n');
-    }
-  return (0);
+int i, c;
+for (i = 0; i <= 9; i++)
+{
+for (c = 97; c <= 122; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return (0);
 }
