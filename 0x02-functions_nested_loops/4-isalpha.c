64a9a84570fb29755750bf66db64e62c5e9bb97f
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-09 11:24:11 +0000

    letters in lower and upper case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _isalpha - isalpha function.
+*@c :integer
+* Return: 1 if c is a letter. 0 if not.
+*/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
