786b28ccae8fb9239f1d85c277b2bca801c9f70d
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-09 09:05:30 +0000

    Holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,4 +1,4 @@
-int _putchar(char c)
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,4 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
