623a1a05aa6fb7f9395e10f01d238873391ec087
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-11 16:56:15 +0000

    numberz

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *@i: integer
- * Return: Always 0.
- */
+* print_numbers - print numbers.
+*
+* Return: Always 0.
+*/
 void print_numbers(void)
 {
 	int i;
 
 	for (i >= 48; i <= 57; i++)
 	{
-	_putchar (i);
+		_putchar (i);
 	}
 	{
-	_putchar ('\n');
+		_putchar ('\n');
 	}
 }
