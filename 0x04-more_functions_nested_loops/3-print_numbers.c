c0b5dad1efe5f62a7571b12f0b324d1ad52e9f3a
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-11 14:26:28 +0000

    print numberz

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+		int i;
+
+		for (i >= 48; i <= 57; i++)
+		{
+			_putchar (n);
+		}
+		{
+	 _putchar ('\n');
+		}
+}
