73f94a497a9f4308c92f337dddb78740037a8955
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-11 21:13:03 +0000

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_triangle - print triangle
+ *@size:integer.
+ * Return;
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+		{
+			i = 0;
+			while (i < size)
+				{
+					j = 0;
+					while (j < size - i - 1)
+						{
+							_putchar(' ');
+							j++;
+						}
+					k = size - i - 1;
+					while (k <= size)
+						{
+							if (k != size)
+								_putchar('#');
+							else
+								_putchar('\n');
+							k++;
+						}
+					i++;
+				}
+		}
+	else
+		{
+			_putchar('\n');
+		}
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* more_numbers - print numbers
+* Return
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j <= 14; j++)
+				{
+					if (j >= 10)
+						{
+							_putchar((j / 10) + '0');
+						}
+					_putchar((j % 10) + '0');
+				}
+			_putchar('\n');
+		}
+}
