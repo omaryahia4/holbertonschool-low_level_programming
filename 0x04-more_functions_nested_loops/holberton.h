bcf7eb892a23714f8febad30f8cf993a4581e0ac
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-11 11:31:44 +0000

    holberton

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
