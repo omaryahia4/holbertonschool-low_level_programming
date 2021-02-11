4afd0e98ca496c3df3c126662d7bab1d0d294df8
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-11 20:50:39 +0000

    numberz

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
 * print_numbers - print numbers.
-*
+*@i: integer
 * Return: Always 0.
 */
 void print_numbers(void)
 {
-	int i;
-
-	for (i >= 48; i <= 57; i++)
+	for (int i >= 48; i <= 57; i++)
 	{
 		_putchar (i);
+	}
+	{
 		_putchar ('\n');
 	}
 }
