eb172ceaf028440c6d5de96e5d702ee4dada14dd
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-16 20:55:23 +0000

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
 
 /**
- * puts2 - print every other character
- * @str: the string
- */
+* puts2 - print every other character
+* @str: the string
+*/
 void puts2(char *str)
 {
 	int i;
 
-		for (i = 0; str[i] != '\0'; i = i + 2)
+	i = 0;
+			while (str[i] != '\0')
 		{
-			_putchar(str[i]);
+				if (i % 2 == 0)
+				{
+					_putchar(str[i]);
+				}
+			i++;
 		}
 	_putchar('\n');
 }
