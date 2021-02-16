5d0855755ce0a781d77493da9d7f4f296b9374d4
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-16 20:31:48 +0000

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 /**
-**_strcpy - copy string into another string
+* *_strcpy - copy string into another string
 *@dest: string
 *@src: string
-*return: dest
+*Return: dest
 */
+
 char *_strcpy(char *dest, char *src)
 {
-	int i, j;
+		int i, j;
 
-	i = 0;
+		i = 0;
 		while (src[i] != '\0')
 		{
 			i++;
 		}
-	j = i;
+		j = i;
 		for (i = 0; i <= j; i++)
 		{
 			dest[i] = src[i];
 		}
-	return (dest);
+		return (dest);
 }
