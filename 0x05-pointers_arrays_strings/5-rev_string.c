ca6fa14100432f96aff2d8fb3ba4d15caf295da4
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-16 18:51:12 +0000

    reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* rev_string - length of string
+*@s:a string
+* Return
+*/
+void rev_string(char *s)
+{
+	char ch;
+	int i, j;
+
+	i = 0;
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+	j = 0;
+	{
+			while (j <= (i - 1) / 2)
+			{
+				ch = s[j];
+				s[j] = s[i - 1 - j];
+				s[i - 1 - j] = ch;
+				j++;
+			}
+	}
+}
