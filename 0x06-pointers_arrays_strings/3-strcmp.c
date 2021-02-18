63f99e160ac211d8b3c8f686f3ead7918df8ffce
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-18 21:26:29 +0000

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strncpy - compare strings
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * Return: 0 lmao
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+		while (s1[i])
+		{
+			if (s1[i] > s2[i])
+				return (s1[i] - s2[i]);
+			else if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			i++;
+		}
+	return (0);
+}
