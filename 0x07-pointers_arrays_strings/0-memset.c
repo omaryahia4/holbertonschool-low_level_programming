ae319649a1bcd6fc77fd3713b68cfa062b69252b
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-22 19:05:39 +0000

    constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+**_memset - fills the first n bytes with the constant byte b
+*@s : pointer
+*@n : integer
+*@b : char
+*Return: *s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
