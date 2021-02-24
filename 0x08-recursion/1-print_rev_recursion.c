e223e7ac3d9c2a6943af8c77250720cb7f0d264b
omaryahia4 <omar.yahia4444@gmail.Com>
2021-02-24 15:01:48 +0000

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - print string in reverse
+*@s: string to be printed
+*Return: 0
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
