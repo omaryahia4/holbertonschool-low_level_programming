bc449d71a1692b558691d6e7cfb984188e290815
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-01 12:31:24 +0000

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print name program
+ *@argc:integer
+ *@argv:string
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
