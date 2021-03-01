ba8e1370a6f539a30dbecc8a7491ca61804a5200
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-01 20:02:36 +0000

    multiplying two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,13 +8,14 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, mul = 1;
+	int i, mul;
 
+	mul = 1;
 	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			mul = mul * atoi(argv[i]);
+			mul *= atoi(argv[i]);
 		}
 			printf("%d\n", mul);
 	}
