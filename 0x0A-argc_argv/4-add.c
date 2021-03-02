9f769ae8202d57df8276191a7d554a7c656b7a49
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-02 08:50:58 +0000

    adding numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,18 +16,19 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	if (argc > 1)
 		{
-			for (i = 1; i < argc; i++)
-				{
-					for (j = 0; argv[i][j] != '\0'; j++)
-						{
-							if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-								{
-									printf("Error\n");
-									return (1);
-								}
-						}
-					Sum = Sum + atoi(argv[i]);
-				}
+		for (i = 1; i < argc; i++)
+		{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+				Sum = Sum + atoi(argv[i]);
+		}
 			printf("%d\n", Sum);
 		}
 	return (0);
+}
