c179b31b23ca57f0a29389729bdd50aa84f7e634
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-03 10:19:16 +0000

    copy of the string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+**_strdup - copy string
+*@str: string
+*Return: str or NULL
+*/
+char *_strdup(char *str)
+{
+	int i, len;
+	char *array;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	array = malloc(sizeof(char) * len + 1);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		array[i] = str[i];
+	}
+	array[i] = '\0';
+	return (array);
+}
