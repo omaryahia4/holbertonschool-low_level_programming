50d74a70824e04a95adc6b02326c9f7bbf1489be
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-08 14:38:24 +0000

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,10 @@ char *name;
 float age;
 char *owner;
 };
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
 
