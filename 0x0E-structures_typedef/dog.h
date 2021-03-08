e8e9e2fbb1f2fdc313847b54a18254444f2ce8ef
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-08 14:20:06 +0000

    structure declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - dog's DATA
+* @name: dog's name
+* @age:  dog's age
+* @owner: dog's owner
+*/
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
-
+#endif
 
