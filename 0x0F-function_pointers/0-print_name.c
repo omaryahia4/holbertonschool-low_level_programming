2a4740ddae3b29e3f50752bc389dadb2c8f37a68
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-10 14:01:38 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - print name
+*@name: pointer
+*@f: pointer of function
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
