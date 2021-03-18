d6471592ed5e500296aa7e3dac66baeec8d039e5
omaryahia4 <omar.yahia4444@gmail.Com>
2021-03-18 19:59:58 +0000

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,12 +2,12 @@
 #define HEADER_FILE
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  * for Holberton project
  */
