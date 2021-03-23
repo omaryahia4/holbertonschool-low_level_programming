cb1641f10a405c6d0528817c01418382c36b818f
omaryahia4 <omar.yahia4444@gmail.com>
2021-03-23 19:11:36 +0100

    holberton

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,9 +14,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
