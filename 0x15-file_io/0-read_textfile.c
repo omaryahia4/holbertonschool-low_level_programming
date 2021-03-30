95efd5e30fadb9f8b31edabc0c0bce3a06989f72
omaryahia4 <omar.yahia4444@gmail.com>
2021-03-30 21:50:10 +0100

    read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* read_textfile - reads a text file and prints it
+* @filename: pointer of the file
+* @letters: size of letters
+* Return: the readFile
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rd, wd;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buf, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (wd);
+}
