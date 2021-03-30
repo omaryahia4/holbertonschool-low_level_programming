91df63c421ae4e05cd5c7605820d42797e843b9e
omaryahia4 <omar.yahia4444@gmail.com>
2021-03-30 22:11:17 +0100

    read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, rd, wd;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters);
+	*buf = malloc(sizeof(char) * letters);
 	if (filename == NULL)
 	{
 		return (0);
