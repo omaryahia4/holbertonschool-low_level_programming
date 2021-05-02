718ce48c6884769b47d0be1ef7486293efb6fa85
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-02 22:59:51 +0100

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,14 +4,14 @@
  *a new node at a given position.
  *@h: head node
  *@idx: index
- *@n: int
+ *@n: Data of node
  *Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode;
 	dlistint_t *tmp;
-	int i = 0;
+	unsigned int i = 0;
 
 	newNode = malloc(sizeof(dlistint_t));
 		if (newNode == NULL)
