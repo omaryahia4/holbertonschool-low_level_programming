273ac32c806691a8f8c30a116f2b54bb6d28e73b
omaryahia4 <omar.yahia4444@gmail.com>
2021-05-02 12:50:55 +0100

    freee

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a linked list
+ *@head: head of a linked list
+ *Return: 0
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	ptr = head;
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	free(ptr);
+}
