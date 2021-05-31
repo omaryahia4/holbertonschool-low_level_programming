092169f25d8b6c53baa65611236b1c3507248798
omaryahia4 <77851226+omaryahia4@users.noreply.github.com>
2021-05-31 12:00:04 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 
 	hash = malloc(sizeof(hash_table_t));
-	if (hash == NULL)	
+	if (hash == NULL)
 		return (NULL);
 	hash->array = malloc(size * sizeof(hash_node_t));
 	if (hash->array == NULL)
