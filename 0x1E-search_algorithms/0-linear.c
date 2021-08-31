4573a11c4dd882c921f835f2cd43e3d2ee078981
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-31 13:07:35 +0100

    fix code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,18 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	if(array == NULL )
+	{
+		return -1;
+	}
+
 	for(i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n" , i, array[i]);
-		if (array[i] == value)
-		{
-			return i;
-		}
+			if (array[i] == value)
+			{
+				return i;
+			}
 	}
-	return -1;
+	return i;
 }
