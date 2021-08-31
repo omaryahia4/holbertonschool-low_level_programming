444f08b676fb6189356c89237ddb2f620f4c06e0
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-31 13:38:44 +0100

    fixn betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,13 +11,16 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (array == NULL)
-		return -1;
-
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n" , i, array[i]);
-		if (array[i] == value)
-				return i;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 	}
-return -1;
+return (-1);
 }
